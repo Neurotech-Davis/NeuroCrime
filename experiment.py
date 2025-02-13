6e526f96e0e24a1efd6a57bb11a651f9d14453fc
Nolan Ching <nolansc303@gmail.com>
2025-02-12 18:04:17 -0800

    added stimulus

--- experiment.py
+++ experiment.py
@@ -1,23 +1,72 @@
 from psychopy import visual, core, event
 from pylsl import StreamInfo, StreamOutlet
+from pathlib import Path
+
+root = Path(__file__).resolve().parent / "Display"
 
 trials = [
-    {"video":, "images": []},  #contains filepaths like "stimuli/video/video.mp4"
-    {"video":, "images": []},
-    {"video":, "images": []},
-    {"video":, "images": []},
-    {"video":, "images": []},
-    {"video":, "images": []}
+    {
+        "video": root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "Leemet-Bockler-High-Clip1.mp4",
+        "images": [
+            root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "2-Kasper-Surorg.png",
+            root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "33-Martin-Meiers.png",
+            root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "9-Severi-Kaukiainen.png",
+            root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "$11-Leemet-Bockler.png",
+            root / "11-Leemet-Bockler-(BC-Kalev)-selected" / "15-Anrijs-Miska.png",
+        ],
+    },
+    {
+        "video": root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "Cooper-Flag-Clip.mp4",
+        "images": [
+            root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "$2-Cooper-Flagg.png",
+            root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "52-Stanley-Borden.png",
+            root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "55-Spencer-Hubbard.png",
+            root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "7-Kon-Knueppel.png",
+            root / "2-Cooper-Flagg-(Duke-Blue-Devils)-selected" / "8-Darren-Harris.png",
+        ],
+    },
+    {
+        "video": root / "23-Stephanie-Mavunga-(Poland)-selected" / "Stephanie-Mavunga-Clip1.mp4",
+        "images": [
+            root / "23-Stephanie-Mavunga-(Poland)-selected" / "$23-Stephanie-Mavunga.png",
+            root / "23-Stephanie-Mavunga-(Poland)-selected" / "12-Liliana-Banaszak.png",
+            root / "23-Stephanie-Mavunga-(Poland)-selected" / "13-Weronika-Gajda.png",
+            root / "23-Stephanie-Mavunga-(Poland)-selected" / "15-Klaudia-Gertchen.png",
+            root / "23-Stephanie-Mavunga-(Poland)-selected" / "4-Julia-Niemojewska.png",
+        ],
+    },
+    {
+        "video": root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "Marcelinho-Huertas-Clip1.mp4",
+        "images": [
+            root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "$9-Marcelinho-Huertas.png",
+            root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "15-Joan-Sastre.png",
+            root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "3-Jaime-Fernandez.png",
+            root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "35-Fran-Guerra.png",
+            root / "9-Marcelinho-Huertas-(La-Laguna-Tenerife)-selected" / "42-Aaron-Doornekamp.png",
+        ],
+    },
+    {
+        "video": root / "9-Terezia-Palenikova-(Slovakia)-selected" / "Terezia-Palenikova-Clip1.mp4",
+        "images": [
+            root / "9-Terezia-Palenikova-(Slovakia)-selected" / "$9-Terezia-Palenikova.png",
+            root / "9-Terezia-Palenikova-(Slovakia)-selected" / "15-Nikola-Kovacikova.png",
+            root / "9-Terezia-Palenikova-(Slovakia)-selected" / "3-Ivana-Jakubcova.png",
+            root / "9-Terezia-Palenikova-(Slovakia)-selected" / "4-Veronika-Remenarova.png",
+            root / "9-Terezia-Palenikova-(Slovakia)-selected" / "88-Natalia-Martiskova.png",
+        ],
+    },
 ]
 
-def createDisplay(trials, trial):
-    video = trials[trial]["video"]
-    return visual.MovieStim3(win, video, size=(640,480))
 
-def createImages(trials, trial):
+def createDisplay(trial):
+    video = trials[trial]["video"]
+    return visual.MovieStim(win, video, size=(640,480))
+
+def createImages(trial):
     relevant_images=[]
-    for i in range(trials[trial]["images"]):
-        relevant_images.append(trials[trial]["images"][i])
+    for i in range(len(trials[trial]["images"])):
+        image =  visual.ImageStim(win, image=trials[trial]["images"][i], pos=(0,0))
+        relevant_images.append(image)
     return relevant_images
 
 def sendMarker(marker):
@@ -29,15 +78,20 @@ outlet = StreamOutlet(info)
 
 win = visual.Window(size=(800,600), color=(0,0,0), units='pix')
 
-for trial in range(trials):
-    display = createDisplay(trials, trial)
-    images = createImages(trials, trial)
+fixation_point = visual.ImageStim(win, image=root / "gray-dot.png", pos=(0,0))
+fixation_point.draw()
+win.flip()
+core.wait(2.5)
 
-    while display.status != visual.FINISHED:
+for trial in range(len(trials)):
+    display = createDisplay(trial)
+    images = createImages(trial)
+
+    while display.status != visual.FINISHED: 
         display.draw()
         win.flip()
 
-    core.wait(5) #edit waiting period
+    core.wait(1) #edit waiting period
 
     for img in images:
         img.draw()
